e88b979f9926341258e5a1ad1d152a3d64310596
Parmita <160781867+0xparomita@users.noreply.github.com>
2026-03-20 21:12:55 +0600

    Create IUniswapV3Pool.sol

---
+++ IUniswapV3Pool.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IUniswapV3Pool {
+    function swap(
+        address recipient,
+        bool zeroForOne,
+        int256 amountSpecified,
+        uint160 sqrtPriceLimitX96,
+        bytes calldata data
+    ) external returns (int256 amount0, int256 amount1);
+
+    function flash(
+        address recipient,
+        uint256 amount0,
+        uint256 amount1,
+        bytes calldata data
+    ) external;
+}
